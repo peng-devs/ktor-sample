769622a6b525500e54fae780bd9f89d775b639d7
Peng <peng.devs@gmail.com>
2019-06-21 15:30:33 +0800

    Reset code indent to 2 spaces

--- src/main/kotlin/peng/devs/App.kt
+++ src/main/kotlin/peng/devs/App.kt
@@ -1,6 +1,8 @@
 package peng.devs
 
 import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CallLogging
 import io.ktor.http.HttpStatusCode.Companion.OK
 import io.ktor.http.HttpStatusCode.Companion.ServiceUnavailable
 import io.ktor.response.respond
@@ -9,24 +11,27 @@ import io.ktor.routing.put
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.slf4j.event.Level
 
 var AVAILABLE = true
 
-fun main(args: Array<String>) {
-    embeddedServer(Netty, 8080) {
-        routing {
-            get ("/") {
-                call.respond(if (AVAILABLE) "Hello, there" else ServiceUnavailable)
-            }
+fun main() {
+  embeddedServer(Netty, 8080) {
+    routing {
+      get ("/") {
+        call.respond(if (AVAILABLE) "Hello, there" else ServiceUnavailable)
+      }
 
-            get ("/health") {
-                call.respond(if (AVAILABLE) OK else ServiceUnavailable)
-            }
+      get ("/health") {
+        call.respond(if (AVAILABLE) OK else ServiceUnavailable)
+      }
 
-            put ("/shutdown") {
-                AVAILABLE = false
-                call.respond(OK)
-            }
-        }
-    }.start(wait = true)
+      put ("/shutdown") {
+        AVAILABLE = false
+        call.respond(OK)
+      }
+    }
+  }.start(wait = true)
 }
